d97ddd3489d850629eeb7308c50a37727444e046
Rehan Pasha <104275485+Rehanpasha732@users.noreply.github.com>
2022-11-17 04:36:08 -0800

    INSTAGRAM-UI REACT-JS
    INSTAGRAM-UI REACT-JS

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from  './Screens/Navbar'
+import Home from './Component/Home'
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Home/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Component/slider.jsx
@@ -0,0 +1,28 @@
+// import { Carousel } from 'antd';
+// import React from 'react';
+// const contentStyle = {
+//   height: '160px',
+//   color: '#fff',
+//   lineHeight: '160px',
+//   textAlign: 'center',
+//   background: '#364d79',
+// };
+// const Slider = () => (
+//   <Carousel effect="fade">
+//     <div>
+//     <div className="stories">hi</div>
+//     </div>
+//     <div>
+//     <div className="stories">hi</div>
+//     </div>
+//     <div>
+//     <div className="stories">hi</div>
+//     </div>
+//     <div>
+//     <div className="stories">hi</div>
+//     </div>
+//   </Carousel>
+// );
+// export default Slider;
+
+
